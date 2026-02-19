3e89938f17e49bde67557e23f926b3eb10e3803b
Ana Luiza Dos Santos Souza <analuizadossantos5@gmail.com>
2026-02-18 23:57:20 -0300

    Criação de um cadastro de produtos

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'uma-chave-muito-louca-da-nebbia'
 
 db = SQLAlchemy(app)
 
